1eb40dc369b4f7e7cdd5334718ec3bedbff25e5f
greenprotein <garciasa@onid.oregonstate.edu>
2018-08-29 16:24:26 -1000

    it outputs something...

--- hw01/hw01.cpp
+++ hw01/hw01.cpp
@@ -1,9 +1,12 @@
-#include <sthio.h>
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+using namespace std;
 
 int main(int argc,char** argv){
-    using namespace std
-    if(argv[3]=='+'){
-        int x = argv[1]+argv[2];
-        printf("Sum is %d",x);
+    if(*argv[3]=='+'){
+        int x = atoi(argv[1])+atoi(argv[2]);
+        cout<<"Sum is "<<x<<endl;
     }
+    return 0;
 }
